913dde5b6fa19f38396efcd2d41e88feb709a483
Martin Kautz <kautz@ocean-insights.com>
2016-04-08 11:14:27 +0200

    Initial Commit

---
+++ CS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CS
+//
+//  Created by Martin Kautz on 08.04.16.
+//  Copyright © 2016 Raketenmann. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CSTests/CSTests.swift
@@ -0,0 +1,36 @@
+//
+//  CSTests.swift
+//  CSTests
+//
+//  Created by Martin Kautz on 08.04.16.
+//  Copyright © 2016 Raketenmann. All rights reserved.
+//
+
+import XCTest
+@testable import CS
+
+class CSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
